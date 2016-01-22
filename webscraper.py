9275a96caf8fff8ae840f58d1359662a4417aec7
SamMonk <monk_sam@hotmail.com>
2016-01-21 23:33:18 -0500

    PyCharm Project
    I created a PyCharm project and I also upgraded python to version 3.5.1
    and used pip to install lxml and requests.

--- webscraper.py
+++ webscraper.py
@@ -4,11 +4,10 @@ import requests
 page = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
 tree = html.fromstring(page.content)
 
-#This will create a list of buyers:
+# This will create a list of buyers:
 buyers = tree.xpath('//div[@title="buyer-name"]/text()')
-#This will create a list of prices
+# This will create a list of prices
 prices = tree.xpath('//span[@class="item-price"]/text()')
 
-
-print 'Buyers: ', buyers
-print 'Prices: ', prices
+print('Buyers: ', buyers)
+print('Prices: ', prices)
